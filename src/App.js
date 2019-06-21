4fe17aa71c33529b1946964039ad213a05cf9ae9
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-06-21 10:01:39 -0500

    clean react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import './App.css';
 
+function App() {
+  return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+  );
+}
 
-
-
+export default App;
